a1815b3f7ad281afd3422f64aca4ff952cc92ffa
jhbang <jhbang@nhnsoft.com>
2022-01-20 12:04:14 +0900

    초기 성공

---
+++ server/src/main/java/com/grpc/server/GrpcServerService.java
@@ -0,0 +1,20 @@
+package com.grpc.server;
+
+import com.grpc.common.lib.HelloReply;
+import com.grpc.common.lib.HelloRequest;
+import com.grpc.common.lib.SimpleGrpc;
+import io.grpc.stub.StreamObserver;
+import net.devh.boot.grpc.server.service.GrpcService;
+
+@GrpcService
+public class GrpcServerService extends SimpleGrpc.SimpleImplBase {
+
+    @Override
+    public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
+        HelloReply reply = HelloReply.newBuilder()
+                .setMessage("Hello ==> " + request.getName())
+                .build();
+        responseObserver.onNext(reply);
+        responseObserver.onCompleted();
+    }
+}
